585752df58807bfbe569953dbdb934de137ae2d2
sreyonpeuy <speuy78@cmc.edu>
2026-09-22 21:08:41 -0700

    test2

--- HTML_Validator.py
+++ HTML_Validator.py
@@ -10,17 +10,23 @@ def validate_html(html):
     >>> validate_html('<strong>example')
     False
     '''
-    tags = _extract_tags(html)
+    try:
+        tags = _extract_tags(html)
+    except ValueError:
+        return False
+
     stack = []
     for tag in tags:
-        if tag[:2] == ('</'):
-            tag_name = tag[2:-1]
+        if tag.startswith('</'):
+            tag_name = tag[2:-1].strip()
             if not stack or stack[-1] != tag_name:
                 return False
             stack.pop()
         else:
-            tag_name = tag[1:-1]
+            tag_content = tag[1:-1].strip()
+            tag_name = tag_content.split()[0]
             stack.append(tag_name)
+
     return len(stack) == 0
 
 
@@ -40,13 +46,21 @@ def _extract_tags(html):
     inside_tag = False
     for char in html:
         if char == "<":
+            if inside_tag:
+                raise ValueError('found < without matching >')
             inside_tag = True
-            current_tag = "<"
-        elif char == ">" and inside_tag:
-            current_tag += ">"
-            tags.append(current_tag)
-            current_tag = ""
+            current_tag.append(char)
+        elif char == ">":
+            if not inside_tag:
+                continue
+            current_tag.append(char)
+            tags.append("".join(current_tag))
+            current_tag = []
             inside_tag = False
         elif inside_tag:
-            current_tag += char
+            current_tag.append(char)
+
+    if inside_tag:
+        raise ValueError('found < without matching >')
+
     return tags
