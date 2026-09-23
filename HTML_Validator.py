1da3bdabe89a84906c7e5d9025c6d7189e98a562
sreyonpeuy <speuy78@cmc.edu>
2026-09-22 20:58:44 -0700

    test1

--- HTML_Validator.py
+++ HTML_Validator.py
@@ -10,12 +10,18 @@ def validate_html(html):
     >>> validate_html('<strong>example')
     False
     '''
-
-    # HINT:
-    # use the _extract_tags function below to generate a list of html tags without any extra text;
-    # then process these html tags using the balanced parentheses algorithm from the stack.py file.
-    # The main difference between your code and the code from class will be that you will have to keep track of not just the 3 types of parentheses,
-    # but arbitrary text located between the html tags.
+    tags = _extract_tags(html)
+    stack = []
+    for tag in tags:
+        if tag[:2] == ('</'):
+            tag_name = tag[2:-1]
+            if not stack or stack[-1] != tag_name:
+                return False
+            stack.pop()
+        else:
+            tag_name = tag[1:-1]
+            stack.append(tag_name)
+    return len(stack) == 0
 
 
 def _extract_tags(html):
@@ -29,3 +35,18 @@ def _extract_tags(html):
     >>> _extract_tags('Python <strong>rocks</strong>!')
     ['<strong>', '</strong>']
     '''
+    tags = []
+    current_tag = ""
+    inside_tag = False
+    for char in html:
+        if char == "<":
+            inside_tag = True
+            current_tag = "<"
+        elif char == ">" and inside_tag:
+            current_tag += ">"
+            tags.append(current_tag)
+            current_tag = ""
+            inside_tag = False
+        elif inside_tag:
+            current_tag += char
+    return tags
